b10a9f54280020debcaafd761595dd2a529a1dee
Beyza Nur Yüksel <68306258+beyzayukseel@users.noreply.github.com>
2024-05-19 18:50:02 +0300

    Initial commit

---
+++ src/test/java/org/com/thy/bootcamp/BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.thy.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
